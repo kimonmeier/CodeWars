e7d4d74eb405350fc9d4f0f651742bd1eb39c61a
Kimon Meier <kimon.meier@gmail.com>
2023-04-12 22:36:07 +0200

    Build Ground for future Branches

---
+++ TestSuite/CodeWarsChallenge.md
@@ -0,0 +1,2 @@
+# Challenge
+Die Challenge Beschreibung
---
+++ TestSuite/CodeWarsTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace TestSuite
+{
+    public class CodeWarsTest
+    {
+        [Test]
+        public void Test()
+        {
+            // TODO: Insert Test Method
+            Assert.Pass();
+        }
+    }
+}
