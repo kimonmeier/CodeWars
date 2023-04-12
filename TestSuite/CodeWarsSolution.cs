1509a1eb6a243385678a2b23520d0e7b21aca76b
Kimon Meier <kimon.meier@gmail.com>
2023-04-12 23:49:52 +0200

    Mögliche Lösung

--- TestSuite/CodeWarsSolution.cs
+++ TestSuite/CodeWarsSolution.cs
@@ -11,8 +11,50 @@ namespace TestSuite
 
         public static string StripComments(string text, string[] commentSymbols)
         {
+            string[] lines = text.Split('\n');
+            StringBuilder outputString = new StringBuilder();
 
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+
+                if (outputString.Length != 0)
+                {
+                    outputString.Append('\n');
+                }
+
+                int firstCommentIndex = line.IndexOfAny(commentSymbols.Select(x => x.First()).ToArray());
+                if (firstCommentIndex != -1)
+                {
+                    line = line.Substring(0, firstCommentIndex).TrimEnd();
+                }
+                outputString.Append(line);
+            }
+
+            return outputString.ToString();
+            /*
+             * Versuch 2 - NOCH SCHLIMMER
+             * StringBuilder stringBuilder = new StringBuilder();
+            string[] splittedText = text.Split(commentSymbols, StringSplitOptions.TrimEntries);
+
+            foreach (string line in splittedText)
+            {
+                if(splittedText.Last() == line)
+                {
+                    continue;
+                }
+
+                int firstNewLineIndex = line.IndexOf('\n');
+                if(firstNewLineIndex == -1)
+                {
+                    stringBuilder.Append(line);
+                    continue;
+                }
+
+                stringBuilder.Append(line.Substring(firstNewLineIndex, line.Length - firstNewLineIndex));
+            }
+
+            return stringBuilder.ToString();*/
         }
-
     }
 }
