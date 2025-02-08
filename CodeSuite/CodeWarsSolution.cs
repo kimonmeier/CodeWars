ff0f567d005fd6a3f4315c40189e73c6682878d3
Kimon Meier <kimon.meier@gmail.com>
2025-02-08 16:39:50 +0100

    Added Solution

--- CodeSuite/CodeWarsSolution.cs
+++ CodeSuite/CodeWarsSolution.cs
@@ -1,8 +1,29 @@
-﻿namespace CodeSuite;
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeSuite;
 
 public sealed class CodeWarsSolution
 {
     public static bool Scramble(string str1, string str2)
     {
+        List<char> scrambledCharacters = str1.ToCharArray().ToList();
+        char[] charactersToFind = str2.ToCharArray();
+
+        for (int i = 0; i < charactersToFind.Length; i++)
+        {
+            char characterToFind = charactersToFind[i];
+
+            char foundCharacter = scrambledCharacters.FirstOrDefault(x => x == characterToFind);
+
+            if (foundCharacter == default(char))
+            {
+                return false;
+            }
+
+            scrambledCharacters.Remove(foundCharacter);
+        }
+
+        return true;
     }
 }
